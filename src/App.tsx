25542e1948a355cfbbf138b94316fa264f72cb65
trung1007 <trungthanhcao.2003@gmail.com>
2025-02-22 22:31:01 +0700

    useSelector and useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reduxLogo from '/redux.svg'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from './redux/store'
+import { decrement, increment } from './redux/counter/counter.slide'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://redux.js.org" target="_blank">
+          <img src={reduxLogo} className="logo" alt="Redux logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Redux Starter</h1>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 20 }}>
+        <h2>Counter: <span>{count}</span> </h2>
+        <div style={{ display: 'flex', flexDirection: 'column', width: '50%', gap: 20 }}>
+          <button onClick={() => dispatch(increment())}>Increase</button>
+          <button onClick={() => dispatch(decrement())}>Decrease</button>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
--- src/redux/counter/counter.slide.ts
+++ src/redux/counter/counter.slide.ts
@@ -1,32 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface CounterState {
-  value: number,
-  status: string
+  value: number;
+  status: string;
 }
 
 const initialState: CounterState = {
   value: 0,
-  status: 'start'
-}
+  status: "start",
+};
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState,
   reducers: {
     increment: (state) => {
-      state.value += 1
+      state.value += 1;
     },
     decrement: (state) => {
-      state.value -= 1
+      state.value -= 1;
     },
     incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
+      state.value += action.payload;
+    },
+    reset: (state) => {
+      state.value = 0;
     },
   },
-})
+});
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, incrementByAmount, reset } = counterSlice.actions;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
