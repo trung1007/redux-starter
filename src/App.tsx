df6930745f7f94a7e1176b047cd8a27428643a42
trung1007 <trungthanhcao.2003@gmail.com>
2025-02-22 22:54:12 +0700

    feat: useAppSelector and useAppDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import { useState } from 'react'
 import reduxLogo from '/redux.svg'
 import './App.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { RootState } from './redux/store'
-import { decrement, increment } from './redux/counter/counter.slide'
+import { decrement, increment, reset } from './redux/counter/counter.slide'
+import { useAppDispatch, useAppSelector } from './redux/hooks'
+import { useSelector } from 'react-redux'
+import { RootState } from '@reduxjs/toolkit/query'
 
 function App() {
 
-  const count = useSelector((state: RootState) => state.counter.value)
-  const dispatch = useDispatch()
+  const count = useAppSelector((state) => state.counter)
+  // useSelector return an object represented a state
+  const dispatch = useAppDispatch()
 
   return (
     <>
       <div>
-        <a href="https://redux.js.org" target="_blank">
-          <img src={reduxLogo} className="logo" alt="Redux logo" />
-        </a>
+        <img src={reduxLogo} className="logo" alt="Redux logo" />
       </div>
       <h1>Redux Starter</h1>
       <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 20 }}>
-        <h2>Counter: <span>{count}</span> </h2>
+        <h2>Counter: <span>{count.value}</span> </h2>
         <div style={{ display: 'flex', flexDirection: 'column', width: '50%', gap: 20 }}>
           <button onClick={() => dispatch(increment())}>Increase</button>
           <button onClick={() => dispatch(decrement())}>Decrease</button>
+          <button onClick={() => dispatch(reset())}>Reset</button>
         </div>
-
       </div>
 
     </>
